f1669472113a9a839282bcf6937c78fed5a113aa
Shashidharak89 <shashidharak334@gmail.com>
2025-06-17 22:33:56 +0530

    dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 
 import Navbar from './components/Navbar';
+import PortfolioDashboard from './components/PortfolioDashboard';
 
 export default function Home() {
   return (
     <>
       <Navbar/>
-     
+     <PortfolioDashboard/>
     </>
   );
 }
