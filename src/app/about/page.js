667a527e6cd290b44da52dd4db3947bef9c348de
Shashidharak89 <shashidharak334@gmail.com>
2025-06-21 16:55:38 +0530

    remove about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,12 @@
 import Footer from 'app/components/Footer';
 import Navbar from '../components/Navbar';
-import AboutPage from './AboutPage';
+
 
 export default function About() {
   return (
     <>
       <Navbar />
-      <AboutPage />
+      
       <Footer />
     </>
   );
