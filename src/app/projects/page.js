a245f49b171268e83a284b0c6b0f53f2c8a78524
Shashidharak89 <shashidharak334@gmail.com>
2025-06-24 14:48:24 +0530

    project page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,14 +1,13 @@
 // app/about/page.js
 import Footer from 'app/components/Footer';
 import Navbar from '../components/Navbar';
-
+import ProjectsPage from './ProjectsPage';
 import ProjectsSection from './ProjectsSection';
 
 export default function Projects() {
   return (
     <>
       <Navbar />
-     
       <ProjectsSection/>
       <Footer/>
     </>
