3a7be89c3710587db8c0bc985eee0b952150b723
Shashidharak89 <shashidharak334@gmail.com>
2025-06-26 14:36:39 +0530

    on way

--- src/backend/models/Blog.js
+++ src/backend/models/Blog.js
@@ -5,15 +5,7 @@ const blogSchema = new mongoose.Schema({
   imageurl: [String],
   description: { type: String, required: true },
   likes: { type: Number, default: 0 },
-  comments: {
-    type: [
-      {
-        text: { type: String, required: true },
-        posted: { type: Date, default: Date.now }
-      }
-    ],
-    default: [],
-  },
+  comments: { type: [String], default: [] },
   posted: { type: Date, default: Date.now },
 });
 
