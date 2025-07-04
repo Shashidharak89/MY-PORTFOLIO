284d27a4bca83c7ecfffef244b1847cba4ee984e
Shashidharak89 <shashidharak334@gmail.com>
2025-07-04 12:53:27 +0530

    github compo

--- src/app/handles/page.js
+++ src/app/handles/page.js
@@ -3,6 +3,7 @@ import Footer from 'app/components/Footer';
 import Navbar from '../components/Navbar';
 import Leetcode from './Leetcode';
 import FooterC from 'app/components/FooterC';
+import Github from './Github';
 
 
 
@@ -11,6 +12,7 @@ export default function Projects() {
     <>
       <Navbar />
       <Leetcode/>
+      <Github/>
       <FooterC/>
     </>
   );
