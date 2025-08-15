550c0f7458b690c433afe8a483f64276ad8ad650
Shashidharak89 <shashidharak334@gmail.com>
2025-08-15 16:44:41 +0530

    Achivements Update

--- src/app/achievements/Data.js
+++ src/app/achievements/Data.js
@@ -1,47 +1,64 @@
 // Data.js - Achievements data storage
 export const achievementsData = [
-  {
-    id: 1,
-    date: '30 Apr 2024',
-    title: 'Runners in Coding',
-    description: 'Secured Runner position in Coding Competition, EKASHUNYAM IT Fest Held at SDM College , Ujire',
-    participants: ['Shashidhara K','Rashmitha P'],
-    images: [
-      'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755249369/Picsart_25-06-22_19-59-26-527_mo1nmu.jpg'
-     
-    ]
-  },
-  {
-    id: 2,
-    date: '29,30 Aug 2024',
-    title: 'Winner in Coding',
-    description: 'Secured First position in Coding Competition, JOSHIANA 13.0 IT Fest Held at ST.Joseph Engineering College, Mangalore',
-    participants: ['Shashidhara K'],
-    images: [
-      'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755249348/Picsart_25-06-22_20-24-09-707_vbfdyl.jpg'  
-    ]
-  },
     {
-    id: 3,
-    date: '26 Mar 2025',
-    title: 'Winners in Coding',
-    description: 'Secured First position in Coding Competition, EKASHUNYAM IT Fest Held at SDM College , Ujire',
-    participants: ['Shashidhara K', 'Rashmitha P'],
-    images: [
-      'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755249350/Picsart_25-06-22_19-43-51-737_nsat8v.jpg',
-      
-    ]
-  },
+        id: 1,
+        date: '30 Apr 2024',
+        title: 'Runners in Coding',
+        description: 'Secured Runner position in Coding Competition, EKASHUNYAM IT Fest Held at SDM College , Ujire',
+        participants: ['Shashidhara K', 'Rashmitha P'],
+        images: [
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755249369/Picsart_25-06-22_19-59-26-527_mo1nmu.jpg',
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755256265/IMG_4909_NjT7vCXx3s_gvsgdw.jpg'
+
+        ]
+    },
     {
-    id: 4,
-    date: '03 Apr 2025',
-    title: 'Runner in Coding',
-    description: 'Runner in Coding Competition, PINNACLE IT Fest Held at ST.Philomina College , Puttur',
-    participants: ['Shashidhara K'],
-    images: [
-      'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755249357/Picsart_25-04-05_18-47-51-801_zvwvso.jpg'
-    ]
-  }
-  
-  
+        id: 2,
+        date: '29,30 Aug 2024',
+        title: 'Winner in Coding',
+        description: 'Secured First position in Coding Competition, JOSHIANA 13.0 IT Fest Held at ST.Joseph Engineering College, Mangalore',
+        participants: ['Shashidhara K'],
+        images: [
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755256265/IMG_9741_Cq1PkwQe4X_s1dviw.jpg',
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755256265/IMG_9812_ndH5p9yK2u_p42ola.jpg',
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755256265/Picsart_24-09-15_09-28-46-275-min_z95itCkw4q_pgqpz4.jpg'
+        ]
+    },
+    {
+        id: 3,
+        date: '14,15 Mar 2025',
+        title: 'Overall Winners',
+        description: 'Winners in TECHOTSAV-2K25 IT Fest Held at Alva\'s College Moodbidri.',
+        participants: ['Team SHC-Neural Knights'],
+        images: [
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755256266/IMG-20250315-WA0008_YtieSope0z_zi5xu5.jpg',
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755256267/IMG-20250316-WA0009_YZwfWnNW3e_h3tefd.jpg'
+
+        ]
+    },
+    {
+        id: 4,
+        date: '26 Mar 2025',
+        title: 'Winners in Coding',
+        description: 'Secured First position in Coding Competition, EKASHUNYAM IT Fest Held at SDM College , Ujire',
+        participants: ['Shashidhara K', 'Rashmitha P'],
+        images: [
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755256264/DSC_3013_OWW6YZRK90_toevaj.jpg',
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755256264/IMG-20250326-WA0038_covHoGCp2N_skvx2b.jpg'
+
+        ]
+    },
+    {
+        id: 5,
+        date: '03 Apr 2025',
+        title: 'Runner in Coding',
+        description: 'Runner in Coding Competition, PINNACLE IT Fest Held at ST.Philomina College , Puttur',
+        participants: ['Shashidhara K'],
+        images: [
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755249357/Picsart_25-04-05_18-47-51-801_zvwvso.jpg',
+            'https://res.cloudinary.com/dsojdpkgh/image/upload/v1755256267/Picsart_25-04-06_11-12-08-700_DkoGl9Qt8o_hmvsvu.jpg'
+        ]
+    }
+
+
 ].reverse();
