fd7503bd213da7950c54de35b659b409b29650f0
Shashidharak89 <shashidharak334@gmail.com>
2025-08-15 11:41:37 +0530

    Achivements route create

---
+++ src/app/achievements/page.js
@@ -0,0 +1,17 @@
+// app/about/page.js
+
+
+import FooterC from 'app/components/FooterC';
+import Navbar from '../components/Navbar';
+
+
+
+export default function Projects() {
+  return (
+    <>
+      <Navbar />
+     
+      <FooterC/>
+    </>
+  );
+}
