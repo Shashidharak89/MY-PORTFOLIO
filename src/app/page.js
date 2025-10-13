5ab4c2bf02ccf600bb0f9401e146763c39cad98e
Shashidharak89 <shashidharak334@gmail.com>
2025-10-13 12:52:21 +0530

    script add adsense

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import { usePathname } from 'next/navigation';
+import Script from 'next/script';
 import * as gtag from '../lib/gtag';
 
 import Navbar from './components/Navbar';
@@ -9,7 +10,6 @@ import PortfolioDashboard from './components/PortfolioDashboard';
 import Preloader from './components/Preloader';
 import FooterC from './components/FooterC';
 import ChatWidget from './components/ChatWidget';
-
 
 export default function Home() {
   const pathname = usePathname();
@@ -22,12 +22,23 @@ export default function Home() {
 
   return (
     <>
+      {/* Google AdSense Script */}
+      <Script
+        id="adsense-script"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4934238485595915"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+
+      {/* Your Components */}
       <Navbar />
       <PortfolioDashboard />
       <Preloader />
-      <ChatWidget/>
-      <FooterC/>
-      
+      <ChatWidget />
+      <FooterC />
+
+    
     </>
   );
 }
